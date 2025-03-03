525866e437519cca13e7044a616d29ab93b5c04b
chaunm-14 <chau_nguyen@fastboy.net>
2025-03-03 04:45:41 -0500

    feat: test ci 5

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'TEST11111'),
+      home: const MyHomePage(title: 'TEST22222'),
     );
   }
 }
